bcc74b2c51266112e62da3b144e35f7199205a90
Maithili Luktuke <56013602+mluktuke@users.noreply.github.com>
2022-01-18 19:04:56 -0800

    Create parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+#include "parser.h"
+
+class ParserException : public exception
+{
+   int m_Pos;
+   
+public:
+   ParserException(const string& message, int pos):
+      exception(message.c_str()),
+      m_Pos(pos)
+   {
+   }
+};
+
+ASTNode* Expression()
+{
+    ASTNode* tnode = Term();
+    ASTNode* e1node = Expression1();
+
+    return CreateNode(OperatorPlus, tnode, e1node);
+}   
+
+ASTNode* Expression1()
+{
+    ASTNode* tnode;
+    ASTNode* e1node;
+
+    switch(m_crtToken.Type)
+    {
+    case Plus:
+        GetNextToken();
+        tnode = Term();
+        e1node = Expression1();
+
+        return CreateNode(OperatorPlus, e1node, tnode);
+
+    case Minus:
+        GetNextToken();
+        tnode = Term();
+        e1node = Expression1();
+
+        return CreateNode(OperatorMinus, e1node, tnode);
+    }
+
+    return CreateNodeNumber(0);
+}
+
+ASTNode* Term()
+{
+    ASTNode* fnode = Factor();
+    ASTNode* t1node = Term1();
+
+    return CreateNode(OperatorMul, fnode, t1node);
+}
+
+ASTNode* Term1()
+{
+    ASTNode* fnode;
+    ASTNode* t1node;
+
+    switch(m_crtToken.Type)
+    {
+    case Mul: 
+        GetNextToken();
+        fnode = Factor();
+        t1node = Term1();
+        return CreateNode(OperatorMul, t1node, fnode);
+
+    case Div:
+        GetNextToken();
+        fnode = Factor();
+        t1node = Term1();
+        return CreateNode(OperatorDiv, t1node, fnode);
+    }
+
+    return CreateNodeNumber(1);
+   }
+
+ASTNode* Factor()
+{
+    ASTNode* node;
+    switch(m_crtToken.Type)
+    {
+    case OpenParenthesis:
+        GetNextToken();
+        node = Expression();
+        Match(')');
+        return node;
+
+    case Minus:
+        GetNextToken();
+		node = Factor();
+        return CreateUnaryNode(node);
+
+    case Number:
+    {
+        double value = m_crtToken.Value;
+        GetNextToken();
+        return CreateNodeNumber(value);
+    }
+
+    default:
+    {
+        stringstream sstr; 
+        sstr << "Unexpected token '" << m_crtToken.Symbol << "' at position " << m_Index;
+        throw ParserException(sstr.str(), m_Index);
+    }
+    }
+}
+
+ASTNode* CreateNode(ASTNodeType type, ASTNode* left, ASTNode* right)
+{
+    ASTNode* node = new ASTNode;
+    node->Type = type;
+    node->Left = left;
+    node->Right = right;
+
+    return node;
+}
+
+ASTNode* CreateUnaryNode(ASTNode* left)
+{
+    ASTNode* node = new ASTNode;
+    node->Type = UnaryMinus;
+    node->Left = left;
+    node->Right = NULL;
+
+    return node;
+}
+
+ASTNode* CreateNodeNumber(double value)
+{
+    ASTNode* node = new ASTNode;
+    node->Type = NumberValue;
+    node->Value = value;
+
+    return node;
+}
+
+void Match(char expected)
+{
+    if(m_Text[m_Index-1] == expected)
+        GetNextToken();
+    else
+    {
+        stringstream sstr;
+        sstr << "Expected token '" << expected << "' at position " << m_Index;
+        throw ParserException(sstr.str(), m_Index);
+    }
+}
+
+void SkipWhitespaces()
+{
+    while(isspace(m_Text[m_Index])) m_Index++;
+}
+
+void GetNextToken()
+{
+    SkipWhitespaces();
+
+	m_crtToken.Value = 0;
+	m_crtToken.Symbol = 0;
+
+    if(m_Text[m_Index] == 0)
+    {
+        m_crtToken.Type = EndOfText;
+        return;
+    }
+
+    if(isdigit(m_Text[m_Index]))
+    {
+        m_crtToken.Type = Number;
+		m_crtToken.Value = GetNumber();
+        return;
+    }
+
+    m_crtToken.Type = Error;
+
+    switch(m_Text[m_Index])
+    {
+    case '+': m_crtToken.Type = Plus; break;
+    case '-': m_crtToken.Type = Minus; break;
+    case '*': m_crtToken.Type = Mul; break;
+    case '/': m_crtToken.Type = Div; break;
+    case '(': m_crtToken.Type = OpenParenthesis; break;
+    case ')': m_crtToken.Type = ClosedParenthesis; break;
+    }
+
+    if(m_crtToken.Type != Error)
+	{
+        m_crtToken.Symbol = m_Text[m_Index];
+        m_Index++;
+	}
+    else
+    {
+        stringstream sstr; 
+        sstr << "Unexpected token '" << m_Text[m_Index] << "' at position " << m_Index;
+        throw ParserException(sstr.str(), m_Index);
+    }
+}
+
+double GetNumber()
+{   
+    SkipWhitespaces();
+
+    int index = m_Index;
+    while(isdigit(m_Text[m_Index])) m_Index++;
+    if(m_Text[m_Index] == '.') m_Index++;
+    while(isdigit(m_Text[m_Index])) m_Index++;
+
+    if(m_Index - index == 0)
+        throw ParserException("Number expected but not found!", m_Index);
+
+    char buffer[32] = {0};
+    memcpy(buffer, &m_Text[index], m_Index - index);
+
+    return atof(buffer);
+}
+
+ASTNode* Parse(const char* text)
+{
+    m_Text = text;
+    m_Index = 0;
+    GetNextToken();
+
+    return Expression();
+}
